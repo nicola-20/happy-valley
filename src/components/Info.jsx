47762bf06385eed9c83d7c2ef0ce6f64837c7820
nicola-20 <nicolalarr@hotmail.co.uk>
2019-07-02 16:39:56 +0100

    sorted out titles and borders on info

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -14,6 +14,12 @@ const supporters = [
   { name: 'Hebden Radio', logo: 'https://www.happyvalleypride.co.uk/wp-content/uploads/2015/12/Sponsor-Hebden-Radio-320x320.png' }
 ];
 
+const FAQs = [
+  { question: 'What is Happy Valley Pride?', answer: 'Answer' },
+  { question: 'Where is Happy Valley Pride?', answer: 'Another answer' },
+  { question: 'Another question that is frequently asked?', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' }
+];
+
 const Info = () => {
   const [activeIndex, setActiveIndex] = React.useState(0)
   console.log(activeIndex)
@@ -22,7 +28,6 @@ const Info = () => {
       title: 'info buttons', content:
         <Box background="neutral-4" fill={true}
           direction="column"
-          justify="evenly"
           align="center"
         >
           <ThemeContext.Extend value={{
@@ -45,18 +50,20 @@ const Info = () => {
               }
             }
           }}>
-            <Box>
-              <Heading level="2" margin="small" alignSelf="left">
+            <Box margin={{top: "large", bottom: "large"}}>
+              <Heading level="2" alignSelf="left" margin={{vertical: "medium"}}>
                 Our Mission
                 </Heading>
-              <Paragraph className="mission" margin="none">
+              <Paragraph className="top-border-paragraph" margin="none">
                 We celebrate LGBT+ life in Hebden Bridge and surrounding areas. Promoting equality and diversity to eradicate discrimination, based on sexual orientation and gender identity, through education and engagement.
                   </Paragraph>
             </Box>
+            <Box direction="column" justify="evenly" align="center" height="50vh">
             <Button className="infoButton" primary={true} label="About Us" onClick={() => setActiveIndex(1)} />
             <Button className="infoButton" primary={true} label="FAQs" onClick={() => setActiveIndex(2)} />
             <Button className="infoButton" primary={true} label="Supporters" onClick={() => setActiveIndex(3)} />
             <Button className="infoButton" primary={true} label="Contact Us" onClick={() => setActiveIndex(4)} />
+            </Box>
           </ThemeContext.Extend>
         </Box>
     },
@@ -65,10 +72,10 @@ const Info = () => {
         <Box background="neutral-4" fill={true} direction="column"
           align="center">
           <Box width="80vw" margin={{ top: "large" }} align="center">
-          <Stack anchor="left" fill="horizontal">
-            <Box fill="horizontal" justify="center"><Heading level="2" margin="none" textAlign="center">About Us</Heading></Box>
-            <Button label={<Heading level="2" margin={{left: "small"}}>&lt;</Heading>} plain={true} margin="none" onClick={() => setActiveIndex(0)} />
-          </Stack>
+            <Stack anchor="left" fill="horizontal">
+              <Box fill="horizontal" justify="center"><Heading level="2" margin={{top: "medium", bottom: "none", horizontal:"none"}} textAlign="center">About Us</Heading></Box>
+              <Button label={<Heading level="2" margin={{ left: "small", bottom: "none", top: "medium" }}>&lt;</Heading>} plain={true} margin="none" onClick={() => setActiveIndex(0)} />
+            </Stack>
           </Box>
           <ThemeContext.Extend value={{
             // button: {}
@@ -90,7 +97,7 @@ const Info = () => {
               }
             }
           }}>
-            <Paragraph>
+            <Paragraph className="top-border-paragraph">
               It all started in response to a piece of homophobic graffiti in the Hebden Bridge Town Centre during 2016, now it’s a annual festival bringing people together.
               Happy Valley Pride was born when local residents witnessed homophobic graffiti sprayed on a piece of tarpaulin by the Hebden Bridge Town Hall, during the summer of 2015. It was a stark reminder that there’s still work to be done – a casual phrase, that has been knocking around school playgrounds for decades, it was none-the-less shocking, simply because it is still in use in our seemingly, tolerant and open-minded community.
           </Paragraph>
@@ -99,30 +106,42 @@ const Info = () => {
     },
     {
       title: 'faqs', content:
-        <Box background="neutral-4" fill={true}>
-          <Box direction="row">
+        <Box background="neutral-4" fill={true} direction="column" align="center">
+          {/* <Box direction="row">
             <Button label="<" plain={true} onClick={() => setActiveIndex(0)} /> <Text>FAQs</Text>
+          </Box> */}
+          <Box width="80vw" margin={{ top: "large" }} align="center">
+            <Stack anchor="left" fill="horizontal">
+              <Box fill="horizontal" justify="center"><Heading level="2" margin={{top: "medium", bottom: "none", horizontal:"none"}} textAlign="center">FAQs</Heading></Box>
+              <Button label={<Heading level="2" margin={{ left: "small", bottom: "none", top: "medium" }}>&lt;</Heading>} plain={true} margin="none" onClick={() => setActiveIndex(0)} />
+            </Stack>
           </Box>
-          <Accordion>
-            <AccordionPanel label="What is Happy Valley Pride?">
-              <Box pad="medium" background="light-2">
-                <Text>One</Text>
-              </Box>
-            </AccordionPanel>
-            <AccordionPanel label="Where is Happy Valley Pride?">
-              <Box pad="medium" background="light-2">
-                <Text>Two</Text>
-              </Box>
-            </AccordionPanel>
-          </Accordion>
-          <Button label="< back" onClick={() => setActiveIndex(0)} />
+          <Box width="80vw">
+            <Accordion className="top-border-paragraph" margin={{"vertical": "medium", "horizontal": "none"}}>
+              {FAQs.map((FAQ) => {
+                return (
+                  <AccordionPanel label={FAQ.question}>
+                    <Box pad="medium" background="light-2">
+                      <Text>{FAQ.answer}</Text>
+                    </Box>
+                  </AccordionPanel>
+                )
+              })}
+            </Accordion>
+          </Box>
         </Box>
     },
     {
       title: 'supporters', content:
-        <Box background="neutral-4" fill={true}>
-          <Box direction="row">
+        <Box background="neutral-4" fill={true} direction="column" align="center">
+          {/* <Box direction="row">
             <Button label="<" plain={true} onClick={() => setActiveIndex(0)} /> <Text>Supporters</Text>
+          </Box> */}
+          <Box width="80vw" margin={{ top: "large" }} align="center">
+            <Stack anchor="left" fill="horizontal">
+              <Box fill="horizontal" justify="center"><Heading level="2" margin="none" textAlign="center">Supporters</Heading></Box>
+              <Button label={<Heading level="2" margin={{ left: "small", bottom: "none" }}>&lt;</Heading>} plain={true} margin="none" onClick={() => setActiveIndex(0)} />
+            </Stack>
           </Box>
           <Box direction="row" wrap={true} align="center" alignContent="around" justify="around" overflow="scroll">
             {supporters.map((supporter) => {
--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,10 +1,53 @@
 import React from 'react';
-import { Box } from 'grommet'
+import { Box, Box as Icons, Anchor as SocialIconLink, Heading, Paragraph, ThemeContext, Image } from 'grommet'
+import { Facebook, Twitter, Instagram } from 'grommet-icons'
+// import InstagramEmbed from 'react-instagram-embed';
+// import InstagramEmbed from 'react-social-embed'
+
+const instagramImages = [
+  "https://scontent-lhr3-1.cdninstagram.com/vp/efe0152004e7a2ba9bb29205d969112b/5DBF5A62/t51.2885-15/sh0.08/e35/s640x640/65822711_522357544969273_4149281244534261233_n.jpg?_nc_ht=scontent-lhr3-1.cdninstagram.com",
+  "https://scontent-lhr3-1.cdninstagram.com/vp/261ea61dcc0c2df9757468ef9c6113f9/5DA20AFA/t51.2885-15/e35/65968329_430142797831950_7354951728648025359_n.jpg?_nc_ht=scontent-lhr3-1.cdninstagram.com",
+  "https://scontent-lhr3-1.cdninstagram.com/vp/85ac9c2861bcde0a0a7fa7cd1e99957b/5DBA26BE/t51.2885-15/sh0.08/e35/s640x640/65720794_653836901751878_7053148590153445397_n.jpg?_nc_ht=scontent-lhr3-1.cdninstagram.com",
+  "https://scontent-lhr3-1.cdninstagram.com/vp/31fc17607da505eaff37bef0359f1b4e/5DAA619C/t51.2885-15/e35/66028711_143821710060463_4812017625892234753_n.jpg?_nc_ht=scontent-lhr3-1.cdninstagram.com&se=8"
+]
+
+const hashtags = [
+  "happyvalleypride", "pride2019", "hebdenbridge"
+]
 
 const Social = () => {
   return (
-    <Box background="neutral-3" fill={true}>
-      SOCIAL
+    <Box background="neutral-3" fill={true} direction="column" align="center">
+      <ThemeContext.Extend value={{
+        paragraph: {
+          medium: {
+            maxWidth: "80vw"
+          }
+        }
+      }}>
+        <Box margin={{ top: "large", bottom: "large" }} width="80vw">
+          <Heading level="2" alignSelf="left" margin={{ vertical: "medium" }}>
+            Social Media
+          </Heading>
+          <Paragraph className="mission" margin="none">
+            Interact with us on social media!                  
+          </Paragraph>
+        </Box>
+        <Icons direction="row" justify="around" width="80vw">
+          <SocialIconLink href="https://www.facebook.com/HappyValleyPride" target="_blank" icon={<Facebook color="white" size="large" />} />
+          <SocialIconLink href="https://twitter.com/appyValleyPride" target="_blank" icon={<Twitter color="white" size="large" />} />
+          <SocialIconLink href="https://www.instagram.com/happyvalleypride/" target="_blank" icon={<Instagram color="white" size="large" />} />
+        </Icons>
+        <Box direction="row" wrap={true} width="80vw" height="80vw" justify="around" align="center">
+        {instagramImages.map((image) => {
+          return (
+        <Box height="35vw" width="35vw">
+            <Image fit="cover" src={image}/>
+        </Box>
+          )
+        })}
+        </Box>
+      </ThemeContext.Extend>
     </Box>
   );
 };
